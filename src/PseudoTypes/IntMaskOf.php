5abbe96f5f79789c2dbf1df33bd4c56bf58da31b
AhJ <amirhosseinjafari8228@gmail.com>
2024-07-20 20:34:40 +0330

    Add *Of type

---
+++ src/PseudoTypes/IntMaskOf.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * This file is part of phpDocumentor.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ *  @link      http://phpdoc.org
+ *
+ */
+
+declare(strict_types=1);
+
+namespace phpDocumentor\Reflection\PseudoTypes;
+
+use phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Types\Integer;
+use phpDocumentor\Reflection\PseudoTypes\AggregatedPseudoType;
+
+/** @psalm-immutable */
+final class IntMaskOf extends AggregatedPseudoType
+{
+    public function __construct(array $types)
+    {
+        parent::__construct($types, '| ');
+    }
+
+    public function underlyingType(): Type
+    {
+        return new Integer();
+    }
+
+    /**
+     * Returns a rendered output of the Type as it would be used in a DocBlock.
+     */
+    public function __toString(): string
+    {
+        return 'int-mask-of<' . parent::__toString() . '>';
+    }
+}
